011b78daa6a77f3552e438b35c9fe80bfbc503ae
Carlucho <shenlong_12@hotmailcom>
2015-11-30 15:09:26 -0430

    paypal added

--- app/views/publications/payments.blade.php
+++ app/views/publications/payments.blade.php
@@ -112,12 +112,14 @@
 					<legend>Pago con <strong>PayPal</strong></legend>
 					<p class="textoPromedio">pasillo24.com le ofrece la facilidad de pagar mediante nuestra cuenta paypal.</p>
 					<div class="col-xs-12">
-						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 							<input type="hidden" name="cmd" value="_s-xclick">
-							<input type="hidden" name="hosted_button_id" value="GPH56GS2CYEEY">
-							<input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+							<input type="hidden" name="hosted_button_id" value="CP4S7VE2F595G">
+							<input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 							<img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
 						</form>
+
+
 					</div>
 				</div>
 				<div class="clearfix"></div>
