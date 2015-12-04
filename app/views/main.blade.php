ab4428961c808e75bb1989e5bf27d0be70570207
Carlucho <shenlong_12@hotmailcom>
2015-12-04 14:27:08 -0430

    to preview

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -328,7 +328,7 @@ margin-top: 0.5em;display:inline-block;">
 	
 	{{ HTML::script('js/jquery.min.js') }}
 	{{ HTML::script('js/bootstrap.min.js') }}
-	{{ HTML::script('js/custom.js?v=0.4')}}
+	{{ HTML::script('js/custom-preview.js?v=0.4')}}
 	
 	{{ HTML::script('js/ckeditor.js') }}
 	{{ HTML::script('js/jquery.ckeditor.js') }}
