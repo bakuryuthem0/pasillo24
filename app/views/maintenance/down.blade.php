f65a67738f0104d53f72782b914dc761e15579e2
Carlucho <shenlong_12@hotmailcom>
2016-01-29 14:19:59 -0430

    time again again

--- app/views/maintenance/down.blade.php
+++ app/views/maintenance/down.blade.php
@@ -1,6 +1,6 @@
 <?php 
 	date_default_timezone_set('America/La_Paz');
-	$time = strtotime('30 January 2016 1:00:00');
+	$time = strtotime('30 January 2016 -5 hours');
 	$now  = time();
 	$dif  = $time-$now;
 	$date = date('d-m-Y G:i:s',$dif);
